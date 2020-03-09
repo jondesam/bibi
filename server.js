7d71044d37ef054831c8c5b46273cc4a8da19c72
jonghyun <godaiys@hotmail.com>
2020-03-09 14:50:21 -0400

    favicon added | prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 // var proxy = require('http-proxy-middleware');
+const path = require('path');
 
 const app = express();
 
@@ -15,15 +16,24 @@ app.use(express.json({ extended: false }));
 //   proxy({ target: 'http://www.example.org', changeOrigin: true })
 // );
 
-app.get('/', (req, res) => res.send('API Running'));
+// app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
 app.use('/api/bibim', require('./routes/api/bibim'));
+
+//Serve static asset in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
